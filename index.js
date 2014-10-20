e22c4914d79b228a7a2c31250f7601f5ca59dab2
Antti Hietasaari <antti.hietasaari@cs.helsinki.fi>
2014-10-20 20:17:27 +0300

    added css

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 var express = require('express')
+var fs = require('fs')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var indexpage
+var quoteController
+var style
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	
+	response.send(indexpage)
 })
 
-app.get('/a', function(request, response) {
-  response.send('a')
+app.get('/quoteController.js', function(request, response) {
+  response.send(quoteController)
+})
+
+app.get('/style.css', function(request, response) {
+  response.type("text/css")
+  response.send(style)
 })
 
 app.listen(app.get('port'), function() {
+  fs.readFile('frontend.html', 'utf-8', function (err, data) {
+		if (err) indexpage = "Error reading index page: " + err;
+		else indexpage = data
+  })
+  fs.readFile('quoteController.js', 'utf-8', function (err, data) {
+		if (err) indexpage = "Error reading quote controller script: " + err;
+		else quoteController = data
+  })
+  fs.readFile('style.css', 'utf-8', function (err, data) {
+		if (err) indexpage = "Error reading style sheet: " + err;
+		else style = data
+  })
   console.log("Node app is running at localhost:" + app.get('port'))
+  
 })
