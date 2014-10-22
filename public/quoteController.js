56f05f3259f640df9b7b00d0891c096d6e936182
Antti Hietasaari <antti.hietasaari@cs.helsinki.fi>
2014-10-22 10:04:28 +0300

    moved static content to public

---
+++ public/quoteController.js
@@ -0,0 +1,13 @@
+function quoteController($scope, $http) {
+	$http.get('quotes.json').then(function(res){
+		$scope.quotes = res.data
+		$scope.getRandom = function() {
+			var index = Math.floor((Math.random() * $scope.quotes.length))
+			$scope.quote = $scope.quotes[index].content
+			$scope.author = "-" + $scope.quotes[index].author
+		}
+		
+		$scope.getRandom()
+	})	
+	
+}
